455e67a1b7169fffa6cf7a985da55145ca81c322
Keeno <160424@student.horizoncollege.nl>
2022-10-05 16:09:03 +0200

    progress so far

--- BarberTime/BarberTime/Models/CreateAccount.cs
+++ BarberTime/BarberTime/Models/CreateAccount.cs
@@ -1,19 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using MaxLengthAttribute = System.ComponentModel.DataAnnotations.MaxLengthAttribute;
 
 namespace BarberTime.Models
 {
+    [Table("createAccount")]
     public class CreateAccount
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+
+        [MaxLength(250)]
+        [Required]
         public string Naam { get; set; }
 
+        [MaxLength(250)]
+        [Required]
         public string Email { get; set; }
 
-        public string Wachtwoord { get; set; }    
+        [MaxLength(250)]
+        [Required]
+        public string Wachtwoord { get; set; }
 
         public string TelefoonNummer { get; set; }
 
