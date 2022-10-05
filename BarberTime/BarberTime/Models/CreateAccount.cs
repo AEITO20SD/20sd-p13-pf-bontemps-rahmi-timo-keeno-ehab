0d26b8b00bc5891a3e42b1d0f987e71562ed8f6b
Keeno <160424@student.horizoncollege.nl>
2022-10-05 15:14:36 +0200

    brancg aangemaakt voor create account

---
+++ BarberTime/BarberTime/Models/CreateAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarberTime.Models
+{
+    public class CreateAccount
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+
+        public string Email { get; set; }
+
+        public string Wachtwoord { get; set; }    
+
+        public string TelefoonNummer { get; set; }
+
+
+    }
+}
