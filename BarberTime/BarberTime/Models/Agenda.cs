dc56cb9b2441ebd6f87d7c876e5919cc3d967995
Rahmi tas <159159@student.horizoncollege.nl>
2022-10-12 15:11:40 +0200

    Model aangemaakt

--- BarberTime/BarberTime/Models/Agenda.cs
+++ BarberTime/BarberTime/Models/Agenda.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using MaxLengthAttribute = System.ComponentModel.DataAnnotations.MaxLengthAttribute;
 
 namespace BarberTime.Models
 {
-    internal class Agenda
+    [Table("Agenda")]
+    public class Agenda
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(250)]
+        [Required]
+        public string DateTime { get; set; }
     }
 }
