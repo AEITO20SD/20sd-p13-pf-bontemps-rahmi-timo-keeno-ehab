625994115ce6f0fe761c290dc78e2b1871837214
Keeno <160424@student.horizoncollege.nl>
2022-10-12 12:48:19 +0200

    progress so far

--- BarberTime/BarberTime/Views/Createaccount.xaml.cs
+++ BarberTime/BarberTime/Views/Createaccount.xaml.cs
@@ -1,9 +1,30 @@
-namespace BarberTime.Views;
+using BarberTime.Models;
+using Java.Lang;
+using System.Collections.Generic;
+using static Java.Util.Jar.Attributes;
 
-public partial class Createaccount : ContentPage
+namespace BarberTime;
+
+public partial class CreateAccount : ContentPage
 {
-	public Createaccount()
-	{
-		InitializeComponent();
-	}
+    public CreateAccount()
+    {
+        InitializeComponent();
+    }
+
+    public void OnNewButtonClicked(object sender, EventArgs args)
+    {
+        statusMessage.Text = "";
+
+        App.AccountRepo.AddNewPerson(name.Text, email.Text, password.Text, number.Text);
+        statusMessage.Text = App.AccountRepo.StatusMessage;
+    }
+
+    public void OnGetButtonClicked(object sender, EventArgs args)
+    {
+        statusMessage.Text = "";
+
+        List<CreateAccount> people = App.AccountRepo.GetAllPeople();
+        peopleList.ItemsSource = people;
+    }
 }
