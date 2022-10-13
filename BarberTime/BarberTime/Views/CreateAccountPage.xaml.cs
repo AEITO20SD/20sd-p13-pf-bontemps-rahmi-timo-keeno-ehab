003b85b0e54020870afa5d33ad713ee17c198848
Keeno <160424@student.horizoncollege.nl>
2022-10-13 12:11:49 +0200

    Create account page without changes

--- BarberTime/BarberTime/Views/CreateAccountPage.xaml.cs
+++ BarberTime/BarberTime/Views/CreateAccountPage.xaml.cs
@@ -1,4 +1,6 @@
 namespace BarberTime.Views;
+using BarberTime.Models;
+using System.Collections.Generic;
 
 public partial class CreateAccountPage : ContentPage
 {
@@ -6,4 +8,19 @@ public partial class CreateAccountPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    public void OnNewButtonClicked(object sender, EventArgs args)
+    {
+        statusMessage.Text = "";
+
+        App.AccountRepo.AddNewPerson(name.Text, email.Text, password.Text, number.Text);
+        statusMessage.Text = App.AccountRepo.StatusMessage;
+    }
+
+    public void OnGetButtonClicked(object sender, EventArgs args)
+    {
+        statusMessage.Text = "";
+
+        List<CreateAccount> people = App.AccountRepo.GetAllPeople();
+        peopleList.ItemsSource = people;
+    }
 }
