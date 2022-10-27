b8381c7116e5d46b9590917131b0ec690c11e53a
Rahmi tas <159159@student.horizoncollege.nl>
2022-10-27 15:18:14 +0200

    Button aangemaakt op de agenda pagina

--- BarberTime/BarberTime/Models/Product.cs
+++ BarberTime/BarberTime/Models/Product.cs
@@ -8,7 +8,7 @@ namespace BarberTime.Models
 {
     internal class Product
     {
-        public int Id { get; set; };
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int InvAmount { get; set; }
