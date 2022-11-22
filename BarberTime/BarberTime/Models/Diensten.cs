c8b17424b65033ed5bf2ff29b23c91d5fc49d4d7
Keeno <160424@student.horizoncollege.nl>
2022-11-22 10:49:41 +0100

    Models aangemaakt voor diensten

--- BarberTime/BarberTime/Models/Diensten.cs
+++ BarberTime/BarberTime/Models/Diensten.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MaxLengthAttribute = SQLite.MaxLengthAttribute;
 
 namespace BarberTime.Models
 {
+    [Table("Dienzten")]
     internal class Diensten
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Length { get; set; }
+
+        [MaxLength(250)]
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Prijs { get; set; }
     }
 }
